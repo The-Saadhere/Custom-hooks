d575e4e0b2a705b1f476392f5a32980e3043d213
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2026-07-13 14:10:53 +0500

    Added: Product Card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+import type { Product } from "../data/products";
 
-function ProductCard() {
+type ProductProps = {
+  product: Product;
+  onAddToCart: (productId: number) => void;
+};
+
+function ProductCard({ product, onAddToCart }: ProductProps) {
   return (
-    <div>ProductCard</div>
-  )
+    <div className="product-card">
+      <h3>{product.name}</h3>
+      <p className="price">${product.price}</p>
+      <button aria-label={`Add ${product.name} to cart`} onClick={() => onAddToCart(product.id)}>
+        <FaShoppingCart /> Add to cart
+      </button>
+    </div>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
